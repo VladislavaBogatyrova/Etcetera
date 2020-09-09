ab781c633040848aa67e164de860dcb448415a14
vladislava_9009 <vladislava.bogaturova@gmail.com>
2020-09-09 21:37:48 +0300

    feat: add photo screen

--- src/screens/personal-info/personal-info.tsx
+++ src/screens/personal-info/personal-info.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Text, Image, ImageBackground, StyleSheet, View,} from 'react-native';
+import { Text, Image, StyleSheet, View,} from 'react-native';
 import images from '../../assets/images';
 import { Button, Input } from '../../components';
 import { useNavigation } from '@react-navigation/native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export const PersonalInfo = () => {
   const { navigate } = useNavigation();
